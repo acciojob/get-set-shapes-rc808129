663f9766e2cfdecee57bda1109ed5a1eabf2c4b6
Raj Chaurasiya <rc808129@gmail.com>
2025-10-30 00:20:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
-//complete this code
 class Rectangle {
 	constructor(width, height){
-		this.width = width;
-			this.height = height;
+		this._width = width; // Use underscore to avoid conflict with getter
+		this._height = height; // Use underscore to avoid conflict with getter
 	}
+
 	get width(){
-		return this.width;
+		return this._width; // Return the stored value
 	} 
+
 	get height(){
-		return this.height;
+		return this._height; // Return the stored value
 	}
+
 	getArea(){
-		return this.width * this.height;
+		return this.width * this.height; // Use getters to get width and height
 	}
 }
 
 class Square extends Rectangle {
-	constructor(width, height, side){
-		super(width,height);
-		this.side = side;
-		
+	constructor(side){
+		super(side, side); // Call Rectangle constructor with side for both width and height
 	}
+
 	getPerimeter(){
-		return 4*this.side;
+		return 4 * this.width; // Use width (or height) since both are the same
 	}
 }
 
